21296b5b7c229c4cfc55ff3b69e4c229e2bed84c
Antony Fagundez <jhonfagundez@gmail.com>
2021-05-14 18:47:52 -0300

    Proyecto inicial

---
+++ src/fagundez-antony-1erParcial.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : fagundez-antony-1erParcial.c
+ Author      : Antony Fagundez
+ Version     :
+ Copyright   : Primer Parcial 1G
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
